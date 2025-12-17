df8465495eeb97ada3df9e57a8f0b185f2180863
vishveshwaran1 <vichu2395@gmail.com>
2025-12-17 23:26:26 +0530

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		fontFamily: {
+        instrument: ['"Instrument Serif"', 'serif'],
+      },
 		extend: {},
 	},
 	plugins: [],
