086808781bf87dbbaea16a5d6d9f9d65763897a3
DharShan07vk <dharshanvk07@gmail.com>
2025-12-30 15:49:18 +0530

    Add manual chunking configuration for build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,20 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // vite.config.js
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+
   plugins: [react()],
   resolve: {
     alias: {
