3c8867903ed1ab19fbda542bca8547415496e768
DharShan07vk <dharshanvk07@gmail.com>
2026-03-28 23:22:37 +0530

    remove unused build configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,6 @@ import path from 'path';
 export default defineConfig({
   // vite.config.js
 
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
-
   plugins: [react()],
   resolve: {
     alias: {
